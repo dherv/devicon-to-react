2e8c4844f42b3ec7f51df9106b710868a83e3c49
dherv <damien.herv@gmail.com>
2019-03-23 08:52:10 +0900

    initial nodejs script

---
+++ README.md
@@ -0,0 +1,9 @@
+## Devicon
+
+Devicon is a collection of svg related to programming languages
+http://konpa.github.io/devicon/
+
+## Devicon to react
+
+It will generate two react .tsx files to use in another project.
+All svg are in a 24x24 format with one fill color
---
+++ devicon.ts
@@ -0,0 +1,118 @@
+const fs = require("fs");
+
+class DeviconScript {
+  fileHeaderString: string;
+  list: string[];
+  svgList: string[];
+  constructor() {
+    this.fileHeaderString =
+      'import React, { SFC } from "react"; import styled from "styled-components"; const SVG = styled.svg` height: 24; width: 24; fill: #424242; stroke: #424242;`;';
+    this.list = [];
+    this.svgList = [];
+    this.generate();
+  }
+  private generate() {
+    this.writeHeader().then(() => {
+      this.writeList();
+    });
+  }
+
+  private chooseFile(files, resolve, folder) {
+    const line = files.filter(file => file.match(/-line.svg$/))[0];
+    const plain = files.filter(file => file.match(/-plain.svg$/))[0];
+    if (line) {
+      this.writeSVG("-line", line, resolve, folder);
+    } else if (plain) {
+      this.writeSVG("-plain", plain, resolve, folder);
+    } else {
+      const original = files.filter(file => file.match(/-original.svg$/))[0];
+      if (original) {
+        this.writeSVG("-original", original, resolve, folder);
+      }
+    }
+  }
+  private getFileName(file, fileName) {
+    const name = file
+      .replace(".svg", "")
+      .replace(fileName, "")
+      .replace("-", "");
+
+    return name;
+  }
+  private buildPath(data) {
+    let path = data.match(/(<path(.|\s)*?>)/g);
+
+    if (!path) return;
+    path = path.length > 1 ? path.join("") : path[0];
+
+    const classMatch = path.match(/class=".*?"/g);
+    if (classMatch) {
+      path = path.replace(/class=".*?"/g, "");
+    }
+
+    const fill = path.match(/fill=".*?"/g);
+    if (fill) path = path.replace(/fill=".*?"/g, "");
+    return path;
+  }
+
+  private writeList = () => {
+    const iconFileHeader = `import {${this.list.join()}} from "./Icon_functions"; import {IIcon} from "../Interfaces"; export const Icon: IIcon = {${this.list.join()}};`;
+    fs.writeFile(
+      `../roadskill-react-ts/src/Icons/Icon.tsx`,
+      iconFileHeader,
+      (err, data) => {
+        if (err) console.log(err);
+      }
+    );
+  };
+
+  public writeHeader() {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(
+        `../roadskill-react-ts/src/Icons/Icon_functions.tsx`,
+        this.fileHeaderString,
+        (err, data) => {
+          if (err) console.log(err);
+        }
+      );
+      return fs.readdir("./devicon-master/icons", (err, folder) => {
+        // for each folder get the files
+        folder.forEach(folder => {
+          fs.readdir(`./devicon-master/icons/${folder}`, (err, files) => {
+            this.chooseFile(files, resolve, folder);
+          });
+        });
+      });
+    });
+  }
+  private writeSVG(fileName, file, resolve, folder) {
+    fs.readFile(
+      `../devicon-master/icons/${folder}/${file}`,
+      "utf8",
+      (err, data) => {
+        if (err) throw err;
+        const path = this.buildPath(data);
+
+        const jsx = `<SVG viewBox="0 0 128 128" height="24" width="24">${path}</SVG>`;
+
+        const name = this.getFileName(file, fileName);
+
+        const dataToWrite = `export const ${name}: React.StatelessComponent<{}> = () => (${jsx})\n`;
+
+        !this.list.includes(name)
+          ? this.list.push(name) &&
+            fs.appendFile(
+              `../roadskill-react-ts/src/Icons/Icon_functions.tsx`,
+              dataToWrite,
+              (err, data) => {
+                if (err) console.log(err);
+                return this.list;
+              }
+            )
+          : null;
+      }
+    );
+  }
+}
+
+new DeviconScript();
